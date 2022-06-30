5f87e68dc6074a40db692dfa8b185e5358e3d077
Roger Camargo <huogerac@gmail.com>
2022-06-30 16:04:52 -0300

    Adiciona template revisados

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.pagina_inicial),
+    path('', views.pagina_inicial, name='cursos.inicio'),
+    path('listar/meus-todos/', views.listar_cursos, name='cursos.listar.tudo'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def pagina_inicial(request):
     return render(request, "cursos/pagina_inicial.html")
+
+
+def listar_cursos(request):
+    return render(request, 'cursos/listar_cursos.html')
