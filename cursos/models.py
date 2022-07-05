1e21450f28fa3f3a08a7856774f1170d3d9400b5
Roger Camargo <huogerac@gmail.com>
2022-07-05 12:20:05 -0300

    Atualiza com model de Cursos (aula 06)

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+
 
 # Create your models here.
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField(max_length=256, unique=True, null=True, blank=True)
+    descricao = models.TextField()
+    autor = models.CharField(max_length=128, default="EU", null=True, blank=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+
+    def save(self, *args, **kwargs):
+        # na primeira vez
+        if not self.id:
+            self.slug = slugify(self.nome)
+        super(Curso, self).save(*args, **kwargs)
